ec69f3b7eafa7849fa401db3adcc2841aab61691
Hafeez Ullah <72004991+Hafeezullahkakar@users.noreply.github.com>
2023-01-11 16:16:39 +0500

    Update StringsQuestions.js

--- StringsQuestions.js
+++ StringsQuestions.js
@@ -45,3 +45,34 @@ var reverseString = function(s) {
     
 };
 
+
+// question 4  GoodString?   (occurance of each character is same)
+
+
+function goodString(s){
+    
+    let prevOccur=1;
+    //a b c a b c 
+    for(let i=0; i<s.length-1; i++){
+        let currentOccur =1;
+        for(j=i+1; j<s.length; j++){
+             if(s[i] == s[j]){
+                 currentOccur++;
+             }
+        }
+         if(prevOccur==1){
+             console.log("prev occur first: ", prevOccur, currentOccur)
+         prevOccur = currentOccur;
+             console.log("prev occur last: ", prevOccur ,currentOccur)
+         continue;
+         }
+       
+             console.log("prev occur last: ", prevOccur ,currentOccur)
+    }
+ 
+ 
+}
+
+goodString("abab")
+
+
